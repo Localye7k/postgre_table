b75b69b272cefab864fbd87c7b3c43edeecab6c0
unknown <pedro.costa7k@gmail.com>
2023-08-08 19:50:47 -0300

    commit

---
+++ src/main/java/com/localyes_group/rest_api/RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.localyes_group.rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+}
